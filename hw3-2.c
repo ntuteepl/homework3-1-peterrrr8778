a70c0341806175cee726fbc214f1118697b7ba9f
peterrrr8778 <146065025+peterrrr8778@users.noreply.github.com>
2023-10-19 00:23:27 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int go1, back1, go2, back2, go3, back3;
+    scanf("%d %d %d %d %d %d", &go1, &back1, &go2, &back2, &go3, &back3);
+    int car=1;
+    if(back1>go2||back2>go3)
+    {
+    car++;
+    }
+    printf("%d",car);
+}
